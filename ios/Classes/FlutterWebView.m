2b4cfd5be6ccb715e48ea472e92cb8384c5bd184
thuycom205 <luuthuy205@gmail.com>
2019-11-13 11:10:32 +0700

    update the message

--- ios/Classes/FlutterWebView.m
+++ ios/Classes/FlutterWebView.m
@@ -90,7 +90,7 @@ FlutterEventSink eventSinkSecond;
     [_webView evaluateJavaScript:@"document.documentElement.outerHTML.toString()" completionHandler:^(id html, NSError *error){
         
         NSString *htmlContent = (NSString*)html;
-        if ( [htmlContent containsString:@"trung"]) {
+        if ( [htmlContent containsString:@"gthành công"]) {
              eventSinkSecond(@"checkoutsucess");
             [self->timer invalidate];
             self->timer = nil;
